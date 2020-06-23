14de423c78ce8ea877264be97ec7d8bfbbfb54a7
Joan González <joan.gs@outlook.com>
2020-06-22 20:20:18 -0500

    js para abrir el menú de la barra de búsqueda cuando se ingrese texto y se presione enter

--- scripts/search.js
+++ scripts/search.js
@@ -1,4 +1,13 @@
+//Botón de buscar
 $('.search-button').click(function(){
     console.log('click');
     $('.menu-input').toggle();
+});
+
+//Resultado al poner texto en input
+$('.search').keydown(function(){
+    if(event.key === 'Enter' && $('.search').val() !== ''){
+        console.log('ingreso: ', $('.search').val());
+        $('.menu-input').toggle(true);
+    } 
 });
