e5ce32a0744b4787cc45c44599a5a71717904037
Joan Gonzáález <joan.gs@outlook.com>
2020-07-12 01:58:45 -0500

    agregué comentario en rojo para recordar que debo preguntar cómo hacer funcionar el localStorage

--- scripts/historial.js
+++ scripts/historial.js
@@ -5,15 +5,19 @@ section.classList.add('section-historial');
 
 let searcherSection = document.getElementById('searcher');
 searcherSection.appendChild(section);
+//inicalizo j en 0 para ir indicando qué número de objeto cada vez que se de click
+
+//!no estoy seguro cómo hacer con el local storage
+let j = 0;
 
 function historialDivBelow(inputTextValue){
-    console.log('entro');
     let div = document.createElement('div');
     div.classList.add('button-historial');
     let p = document.createElement('p');
     div.appendChild(p);
-
     p.innerHTML = `#${inputTextValue}`;
-
     section.appendChild(div);
+    //almaceno las búsquedas en localStorage
+    window.localStorage.setItem(`historial${j}`,`${inputTextValue}`);
+    j++;
 }
