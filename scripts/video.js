bdd8008b66859e98256edb0d90132904501b962c
Joan <joan.gs@outlook.com>
2020-09-11 09:35:00 -0500

    agregando funcionalidades a crear guifo

--- scripts/video.js
+++ scripts/video.js
@@ -1,6 +1,18 @@
 let recorderVideo = document.getElementById('recorder');
 let captureButton = document.getElementById('capture-button');
+let cameraButton = document.getElementById('camera-button');
+let camera = document.getElementById('camera');
+let previewGif = document.getElementById('preview-gif');
+let previewMiniGif = document.getElementById('preview-mini-gif');
 let streaming = false;
+
+let stream;
+let recorder;
+let blob;
+let form;
+let urlBlobPreview;
+let dataPostId;
+let gifLink;
 //Evento para capturar el video
 captureButton.addEventListener('click', ()=>{
     if(streaming){
@@ -16,13 +28,15 @@ captureButton.addEventListener('click', ()=>{
 });
 //Play record
 async function getStreamAndRecord(constraints){
-    let stream = null;
+    stream = null;
     try{
         streaming = true;
         stream = await navigator.mediaDevices.getUserMedia(constraints);
         
         recorderVideo.srcObject = stream;
-        recorderVideo.play();
+        // recorderVideo.play();
+        //indico un mensaje antes del STOP
+        captureButton.innerHTML = "Espera...";
         //Defino configuraciones del gif a grabar
         recorder = RecordRTC(stream, {
             type: 'gif',
@@ -32,8 +46,14 @@ async function getStreamAndRecord(constraints){
             width: 360,
             hidden: 240,
             onGifRecordingStarted: ()=>{ 
-                console.log('started'); 
+                console.log('started');
+                recorderVideo.play();
                 captureButton.innerHTML = "Stop";
+                if(nightTheme){
+                    camera.setAttribute('src','./assets/recording_dark.svg');
+                }else{
+                    camera.setAttribute('src','./assets/recording.svg');
+                };
             }
         });
         //inicia la grabación
@@ -50,35 +70,46 @@ async function getStreamAndRecord(constraints){
 async function stopAndPost(){
     await recorder.stopRecording();
     //detengo la grabación
-    let blob = await recorder.getBlob();
+    blob = await recorder.getBlob();
     //configuramos lo que irá en el body del método post
-    let form = new FormData();
+    form = new FormData();
     form.append('file', blob, 'myGif.gif');
+    // Cambio el estado de streaming
+    streaming = false;
     //realizo el post a giphy
-    try{
-        let urlPost = `https://upload.giphy.com/v1/gifs?api_key=${APIKey}&username=Joangs`;
-        let responsePost = await fetch(urlPost, {
-            method: 'POST',
-            body: form
-        });
-        let dataPost = await responsePost.json();
-        //asigno la id del gif enviado a una variable
-        const dataPostId = dataPost.data.id;
-        //dejo de grabar e indico  el nuevo estado
-        streaming = false;
-        captureButton.innerHTML = "Listo";
-        //desaparezco before-start-box y aparezco vista previa
+    captureButton.innerHTML = "Listo";
+    //apago la cámara
+    stream.getTracks().forEach(track => {
+        track.stop();
+    });
+    //reinicio la cámara y así desaparece el video
+    recorderVideo.load();
+    //envíamos el gif creado a la siguiente sección para que sea posible verlo
+    urlBlobPreview = URL.createObjectURL(blob);
+    previewGif.setAttribute('src',urlBlobPreview);
+    // try{
+    //     let urlPost = `https://upload.giphy.com/v1/gifs?api_key=${APIKey}&username=Joangs`;
+    //     //en el método post, en el body, agrego el form
+    //     let responsePost = await fetch(urlPost, {
+    //         method: 'POST',
+    //         body: form
+    //     });
+    //     let dataPost = await responsePost.json();
+    //     //asigno la id del gif enviado a una variable
+    //     const dataPostId = dataPost.data.id;
+    //     console.log(dataPostId)
+    //     //desaparezco before-start-box y aparezco vista previa
 
         // //!para hacerlo en otra funcion probablemente
         // let urlGet = `https://api.giphy.com/v1/gifs/${dataPostId}?api_key=${APIKey}`;
         // let responseGet = await fetch(urlGet);
         // let dataGet = await responseGet.json();
         // console.log(dataGet)
-    }catch(fail){
-        streaming = false;
-        captureButton.innerHTML = "Don't work";
-        throw new Error(fail);
-    };
+    // }catch(fail){
+    //     streaming = false;
+    //     captureButton.innerHTML = "Don't work";
+    //     throw new Error(fail);
+    // };
 };
 
 
@@ -115,20 +146,54 @@ captureButton.addEventListener('click', ()=>{
 repetirCaptura.addEventListener('click', ()=>{
     preview.style.display = "none";
     mainCreateBox.style.display = "block";
-    captureButton.innerHTML = "Capturar"; 
+    captureButton.innerHTML = "Capturar";
+    if(nightTheme){
+        camera.setAttribute('src','./assets/camera_light.svg');
+    }else{
+        camera.setAttribute('src','./assets/camera.svg');
+    }
+    
 });
 //play al guifo ya grabado
 forwardArrow.addEventListener('click', ()=>{
     //!repite el movimiento del guifo
 });
 //cambio de preview con subir guifo a uploading guifo
-subirGuifo.addEventListener('click', ()=>{
+subirGuifo.addEventListener('click', async ()=>{
     preview.style.display = "none";
     uploading.style.display = "block";
-    setTimeout(() => {
+    // //función de prueba para que aparezca página de carga
+    // //después aparece successCreateBox con la posibilidad de copiar link o descargar gif
+    // setTimeout(() => {
+    //     uploading.style.display = "none";
+    //     successCreateBox.style.display = "block";
+    // }, 5000);
+    try{
+        let urlPost = `https://upload.giphy.com/v1/gifs?api_key=${APIKey}&username=Joangs`;
+        //en el método post, en el body, agrego el form
+        let responsePost = await fetch(urlPost, {
+            method: 'POST',
+            body: form
+        });
+        let dataPost = await responsePost.json();
+        //asigno la id del gif enviado a una variable
+        dataPostId = dataPost.data.id;
+        //asigno el gif al mini preview
+        previewMiniGif.setAttribute('src',urlBlobPreview);
+        //desaparezco uploading y aparezco success
         uploading.style.display = "none";
         successCreateBox.style.display = "block";
-    }, 5000);
+
+        // //!para hacerlo en otra funcion probablemente
+        // let urlGet = `https://api.giphy.com/v1/gifs/${dataPostId}?api_key=${APIKey}`;
+        // let responseGet = await fetch(urlGet);
+        // let dataGet = await responseGet.json();
+        // console.log(dataGet)
+    }catch(fail){
+        streaming = false;
+        captureButton.innerHTML = "Don't work";
+        throw new Error(fail);
+    };
 });
 //Cancelar la subida del guifo
 cancelUpload.addEventListener('click', ()=>{
@@ -139,11 +204,19 @@ cancelUpload.addEventListener('click', ()=>{
 });
 //Descargar guifo
 downloadGuifo.addEventListener('click', ()=>{
-    //!Descargar guifo
+    invokeSaveAsDialog(blob);
 });
 //copiar enlace del guifo
-copyLinkButton.addEventListener('click', ()=>{
-    //!Copiar enlace del guifo
+copyLinkButton.addEventListener('click', async ()=>{
+    let urlGet = `https://api.giphy.com/v1/gifs/${dataPostId}?api_key=${APIKey}`;
+    let responseGet = await fetch(urlGet);
+    let dataGet = await responseGet.json();
+
+    gifLink = dataGet.data.bitly_url;
+
+    await navigator.clipboard.writeText(gifLink);
+
+    alert(`Este es el link que copiaste:): ${gifLink}`);
 });
 //listo
 listo.addEventListener('click', ()=>{
